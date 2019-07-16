3016dbdc5df4bb2e2f37a62da15bd9fe08cf363a
lape15 <larape1993@gmail.com>
2019-07-16 19:55:38 +0100

    Updates

---
+++ precache-manifest.6af9fafc01b474d417323b642bc717e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6da7bed352ad2d49918156d1bdb9ab26",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cdb0861bb2e8cd40c013",
+    "url": "/monsters-rolodex/static/css/main.3a4f5bb1.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "cdb0861bb2e8cd40c013",
+    "url": "/monsters-rolodex/static/js/main.98a64c55.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
